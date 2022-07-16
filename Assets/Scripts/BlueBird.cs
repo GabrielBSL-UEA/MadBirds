e20a27b043c35b3593bd0ec9d2d03dab007af9f5
GabrielBSL-UEA <69216849+GabrielBSL-UEA@users.noreply.github.com>
2022-07-16 13:57:46 -0400

    fastBirdImplementation
    fastBirdImplementation

--- Assets/Scripts/BlueBird.cs
+++ Assets/Scripts/BlueBird.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class BlueBird : Bird
 {
+    [Header("SpeedUp Special")]
+    [SerializeField] private float speedUpForce;
 
+    public override bool ApplyBirdEffect()
+    {
+        if (!base.ApplyBirdEffect())
+        {
+            return false;
+        }
+
+        effectUsed = true;
+        Vector2 currentDirection = _rigidbody2D.velocity.normalized;
+
+        _rigidbody2D.velocity = Vector2.zero;
+        _rigidbody2D.AddForce(currentDirection * speedUpForce);
+
+        return true;
+    }
 }
